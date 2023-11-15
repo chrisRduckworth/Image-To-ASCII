4db3a921d6541fa31db13d9dd4098c4a287a6902
chris <c_duckworth@hotmail.com>
2023-11-15 15:17:21 +0000

    initial commit

---
+++ character_analysis.py
@@ -0,0 +1,19 @@
+class Glyph:
+  def __init__(self, character):
+    self.character = character
+  
+  def generate_array(self):
+    pass
+
+  def compare_array(self, arr):
+    pass
+
+class Alphabet:
+  def __init__(self, name):
+    self.name = name
+  
+  def create_glyphs(self):
+    pass
+
+  def find_optimal_glyph(self, arr):
+    pass
