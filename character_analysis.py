ff7f1c0038a551c2badc7c49f604b1936fa24ef4
chris <c_duckworth@hotmail.com>
2023-11-15 18:39:54 +0000

    complete generate_array method

--- character_analysis.py
+++ character_analysis.py
@@ -1,12 +1,25 @@
 import numpy as np
-
+from PIL import ImageDraw, Image, ImageFont
+from fontTools import ttLib
+from math import ceil
 
 class Glyph:
     def __init__(self, character):
         self.character = character
 
     def generate_array(self, font_path, font_size, dpi):
-        self.img_array = np.array([], dtype=np.bool_)
+        """Creates a boolean array representing an image of the character"""
+        font = ttLib.TTFont(font_path)
+        # Calculate properties of character
+        char_height = ceil(font_size * dpi / 72)
+        char_width = ceil(font_size * dpi / 72 * font.getGlyphSet()[self.character].width / font['head'].unitsPerEm)
+        # Draw character
+        char_image = Image.new("1", (char_width, char_height), color=1)
+        pillow_font = ImageFont.truetype(font_path, char_height)
+        drawing = ImageDraw.Draw(char_image)
+        drawing.text((0,0), self.character, 0, font=pillow_font, spacing=0)
+        # Convert to array
+        self.img_array = np.array(char_image, np.bool_)
 
     def compare_array(self, arr):
         pass
