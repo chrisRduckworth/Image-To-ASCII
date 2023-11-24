dd74db0f65b131c1d40df0cd1470005a52467e01
chris <c_duckworth@hotmail.com>
2023-11-24 15:12:41 +0000

    refactor generate_array to use ImageFont getmask

--- character_analysis.py
+++ character_analysis.py
@@ -1,26 +1,51 @@
 import numpy as np
-from PIL import ImageDraw, Image, ImageFont
-from fontTools import ttLib
+from itertools import chain
+from PIL import ImageFont
+from fontTools.ttLib import TTFont
 from math import ceil
 import string
 
 class Glyph:
-    def __init__(self, character):
+    def __init__(self, character, character_name):
         self.character = character
+        self.character_name = character_name
 
-    def generate_array(self, font_path, font_size, dpi):
+    def generate_array(self, font, pillow_font, font_size, dpi):
         """Creates a boolean array representing an image of the character"""
-        font = ttLib.TTFont(font_path)
-        # Calculate properties of character
         char_height = ceil(font_size * dpi / 72)
-        char_width = ceil(font_size * dpi / 72 * font.getGlyphSet()[self.character].width / font['head'].unitsPerEm)
-        # Draw character
-        char_image = Image.new("1", (char_width, char_height), color=1)
-        pillow_font = ImageFont.truetype(font_path, char_height)
-        drawing = ImageDraw.Draw(char_image)
-        drawing.text((0,0), self.character, 0, font=pillow_font, spacing=0)
-        # Convert to array
-        self.img_array = np.array(char_image, np.bool_)
+        char_width = ceil(font_size * dpi / 72 * font.getGlyphSet()[self.character_name].width / font['head'].unitsPerEm)
+        # whitespace doesn't draw so generate here
+        if self.character == " ":
+            img_array = np.zeros((char_height, char_width), np.bool_)
+            self.img_array = img_array
+            return
+
+        # draw array of character
+        bounding_box = pillow_font.getbbox(self.character, mode="1") 
+        img_array = np.array(pillow_font.getmask(self.character, "1"), np.bool_)
+        img_array = img_array.reshape((-1, bounding_box[2] - bounding_box[0]))
+        
+        # trim width
+        while len(img_array[0]) > char_width:
+            if (len(img_array[0]) - char_width) % 2 == 1:
+                img_array = np.delete(img_array, len(img_array[0]) -1, 1)
+            else:
+                img_array = np.delete(img_array, 0, 1)
+        # trim height:
+        # I don't know if this is necessary but it doesn't hurt
+        # I haven't seen excess height but it might appear somewhere
+        while len(img_array) > char_height:
+            if (len(img_array) - char_height) % 2 == 1:
+                im_array = np.delete(img_array, 0, 0)
+            else:
+                img_array = np.delete(img_array, len(img_array) - 1, 0)
+        
+        # pad top & bottom
+        img_array = np.insert(img_array, [0 for i in range(bounding_box[1])], 0, 0)
+        img_array = np.insert(img_array, [len(img_array) for i in range(char_height - bounding_box[3])], 0, 0)
+        # no need to pad width
+        
+        self.img_array = img_array 
 
     def compare_array(self, arr):
         pass
