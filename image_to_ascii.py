3e27e329eb1068916076cf674e847c26b062f0a6
chris <c_duckworth@hotmail.com>
2023-12-14 21:50:11 +0000

    add get subarray function

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -1,3 +1,19 @@
+from math import ceil
+import numpy as np
+
+def get_subarray(img, coords, dimensions):
+    """returns the subarray of img starting at (x,y)"""
+    x, y = coords
+    length, height = dimensions
+
+    subarray = [row[x:x+length] for row in img[y:y+height]]
+
+    if len(subarray[0]) < length:
+        to_add = [0] * (length - len(subarray[0]))
+        subarray = [[*row, *to_add] for row in subarray]
+
+    return np.array(subarray, dtype = img.dtype)
+
 def main():
   pass
 
