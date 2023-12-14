54ae8b95680601314ab7da19b035bcdd7b2c3831
chris <c_duckworth@hotmail.com>
2023-12-14 18:29:50 +0000

    fix load_image to convert to 1-bit depth image before becoming np array

--- image_processing.py
+++ image_processing.py
@@ -3,9 +3,10 @@ import numpy as np
 
 def load_image(img_name, edge_detection, max_val, min_val):
     """loads the image, runs edge detection"""
-    img = cv.imread(img_name, 0)
+    img = cv.imread(img_name, cv.IMREAD_GRAYSCALE)
     if edge_detection:
         img = cv.Canny(img, max_val, min_val)
-    # we use NOT because black being a 1 makes more sense to me
-    img = ~ np.array(img, np.bool_)
+    # we use INV because black being a 1 makes more sense to me
+    img = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)[1]
+    img = np.array(img, np.bool_)
     return img
