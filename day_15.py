7a4f46d59b07cc4b2def20cd706ccf9450a0c6ae
chris <c_duckworth@hotmail.com>
2023-12-15 12:58:18 +0000

    add calc value function

--- day_15.py
+++ day_15.py
@@ -1,5 +1,12 @@
+from functools import reduce
+
 def calc_new_value(current, char):
     current += ord(char)
     current *= 17
     current %= 256
     return current
+
+def calc_value(seq):
+    """calculates the value of a sequence"""
+    total = reduce(calc_new_value, seq, 0)
+    return total
