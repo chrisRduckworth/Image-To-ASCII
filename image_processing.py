53830fc15be404b316c7a22f79bd4503a99f8147
chris <c_duckworth@hotmail.com>
2023-12-15 17:15:02 +0000

    fix bug with trim whitespace outputting list instead of numpy array

--- image_processing.py
+++ image_processing.py
@@ -6,21 +6,20 @@ def load_image(img_name, edge_detection, max_val, min_val):
     img = cv.imread(img_name, cv.IMREAD_GRAYSCALE)
     if edge_detection:
         img = cv.Canny(img, max_val, min_val)
-    # we use INV because black being a 1 makes more sense to me
-    img = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)[1]
+    img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1]
     img = np.array(img, np.bool_)
     return img
 
 def trim_whitespace(img):
     """removes empty rows/columns"""
     while not any(img[0]):
-        img = img[1:]
+        img = np.delete(img, 0, 0)
     while not any(img[-1]):
-        img = img[:-1]
+        img = np.delete(img, -1, 0)
     while not any(r[0] for r in img):
-        img = [r[1:] for r in img]
+        img = np.delete(img, 0, 1)
     while not any(r[-1] for r in img):
-        img = [r[:-1] for r in img]
+        img = np.delete(img, -1, 1)
     return img
 
 def pad_height(img, char_height):
