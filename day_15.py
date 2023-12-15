7682b1e961d60d391731ff68c9361cc32a30cfee
chris <c_duckworth@hotmail.com>
2023-12-15 12:47:44 +0000

    add day 15

---
+++ day_15.py
@@ -0,0 +1,2 @@
+def calc_new_value(current, char):
+    pass
---
+++ tests/day_15_test.py
@@ -0,0 +1,10 @@
+from day_15 import calc_new_value
+
+class TestCalcNewValue:
+    def test_returns_correct_value_starting_at_zero(self):
+        assert calc_new_value(0, "H") == 200
+
+    def test_returns_correct_value(self):
+        assert calc_new_value(72, "A") == 153
+        assert calc_new_value(200, "S") == 172
+        assert calc_new_value(172, "H") == 52
